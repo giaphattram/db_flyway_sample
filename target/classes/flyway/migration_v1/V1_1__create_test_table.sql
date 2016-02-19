867f3db444109e57efeea4d78e093fc9043c6a8b
Thomas Jaspers <tritom42@googlemail.com>
2016-02-19 07:29:44 +0100

    removed target folder

--- target/classes/flyway/migration_v1/V1_1__create_test_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE flyway_test (
-  key VARCHAR(64),
-  value VARCHAR(255),
-  PRIMARY KEY(key)
-);
-
-ALTER TABLE flyway_test OWNER TO flywaydemo;
--- target/classes/flyway/migration_v1/V1_2__create_test_view.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE VIEW
-    flyway_test_view AS select * from flyway_test;
-
-ALTER VIEW flyway_test_view OWNER TO flywaydemo;
